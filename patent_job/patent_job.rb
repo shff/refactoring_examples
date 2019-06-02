84fea34d972e8e319b6e1b865624347cedf4e0f0
Silvio Henrique Ferreira <shferreira@me.com>
2019-06-02 16:53:39 -0300

    Step 1: Simplify

--- patent_job/patent_job.rb
+++ patent_job/patent_job.rb
@@ -1,28 +1,13 @@
 class PatentJob
   def run
-    temp = download_file
-    rows = parse(temp)
-    update_patents(rows)
-  end
-
-  def download_file
-    temp = Tempfile.new('patents')
-    tempname = temp.path
-    temp.close
-    Net::FTP.open('ftp.ebi.ac.uk', 'anonymous', '') do |ftp|
-      ftp.getbinaryfile("/pub/databases/msd/sifts/csv/pdb_chain_hmmer.csv", tempname)
+    Net::FTP.open('ftp.ebi.ac.uk', 'anonymous') do |ftp|
+      lines = ftp.gettextfile("/pub/databases/msd/sifts/csv/pdb_chain_hmmer.csv", nil)
+      Patent.connection.transaction do
+        Patent.delete_all
+        CSV.parse(lines, headers: true) do |row|
+          Patent.create!(row.to_hash)
+        end
+      end
     end
-    tempname
-  end
-
-  def parse(temp)
-    CSV.read(temp, :headers => true)
-  end
-
-  def update_patents(rows)
-    Patent.connection.transaction {
-      Patent.delete_all
-      rows.each { |r| Patent.create!(r.to_hash) }
-    }
   end
 end
